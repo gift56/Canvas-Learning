595373cdc342b4c65d5b3cc2bd708159c7c5f788
gift56 <efeasiughu@gmail.com>
2022-05-07 19:38:37 +0100

    Successfully draw a smiling face

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,27 +1,75 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
-//drawing rectangle
+// //drawing rectangle
 
-ctx.fillStyle = 'red';
-ctx.fillRect(20, 20, 150, 100);
-ctx.fillStyle = 'blue';
-ctx.fillRect(200, 20, 150, 100);
+// ctx.fillStyle = 'red';
+// ctx.fillRect(20, 20, 150, 100);
+// ctx.fillStyle = 'blue';
+// ctx.fillRect(200, 20, 150, 100);
 
-//next lesson stroke drawing
-ctx.lineWidth = 5;
-ctx.strokeStyle = 'red';
-ctx.strokeRect(100, 150, 150, 100);
+// //next lesson stroke drawing
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = 'red';
+// ctx.strokeRect(100, 150, 150, 100);
 
-//ClearRect()
-ctx.clearRect(25, 25, 140, 90);
+// //ClearRect()
+// ctx.clearRect(25, 25, 140, 90);
 
-// Filltext()
-ctx.font = '30px Arial';
-ctx.fillStyle = 'purple';
-ctx.fillText('Hello World', 400, 50,);
+// // Filltext()
+// ctx.font = '30px Arial';
+// ctx.fillStyle = 'purple';
+// ctx.fillText('Hello World', 400, 50,);
 
-// Stroketext()
-ctx.lineWidth = 1;
-ctx.strokeStyle = 'Purple';
-ctx.strokeText('Hello World', 400, 100);
+// // Stroketext()
+// ctx.lineWidth = 1;
+// ctx.strokeStyle = 'Purple';
+// ctx.strokeText('Hello World', 400, 100);
+
+// Basics ends here
+
+// Paths
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(100, 200);
+// ctx.closePath();
+// // ctx.stroke();
+// ctx.fillStyle = 'orange';
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.moveTo(200, 50);
+// ctx.lineTo(150, 200);
+// ctx.lineTo(250, 200);
+// ctx.lineTo(200, 50);
+// ctx.stroke();
+
+// // Rectangle
+// ctx.beginPath();
+// ctx.rect(300, 50, 150, 100);
+// ctx.fillStyle = 'red';
+// ctx.fill();
+
+// Arc (circles)
+ctx.beginPath();
+const CenterX = canvas.width / 2;
+const CenterY = canvas.height / 2;
+
+// Face Drawing
+
+ctx.arc(CenterX, CenterY, 220, 0, Math.PI * 2);
+
+ctx.moveTo(CenterX + 100, CenterY);
+
+ctx.arc(CenterX, CenterY, 100, 0, Math.PI, false);
+
+ctx.moveTo(CenterX - 60, CenterY - 80);
+
+ctx.arc(CenterX - 80, CenterY - 80, 20, 0, Math.PI * 2);
+
+ctx.moveTo(CenterX + 100, CenterY - 80);
+
+ctx.arc(CenterX + 80, CenterY - 80, 20, 0, Math.PI * 2)
+
+ctx.stroke();
