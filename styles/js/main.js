35bcbcf2d1142c720f4c83acf8ffa4f5c03da84f
gift56 <efeasiughu@gmail.com>
2022-05-07 18:47:43 +0100

    created strokes with canvas

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
+//drawing rectangle
+
 ctx.fillStyle = 'red';
 ctx.fillRect(20, 20, 150, 100);
 ctx.fillStyle = 'blue';
 ctx.fillRect(200, 20, 150, 100);
+
+//next lesson stroke drawing
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'red';
+ctx.strokeRect(100, 150, 150, 100);
